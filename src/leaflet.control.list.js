0b9552c19753dcf442f68c410d570132447456d6
adammertel <mertel.adam@gmail.com>
2018-01-31 19:22:59 +0100

    processing items

--- src/leaflet.control.list.js
+++ src/leaflet.control.list.js
@@ -38,6 +38,60 @@ L.Control.List = L.Control.extend({
     return false;
   },
 
+  _hideMenu: function() {
+    this.state.open = false;
+    this._render();
+  },
+
+  _openMenu: function() {
+    this.state.open = true;
+    this._render();
+  },
+
+  _changeSelected: function(newValue) {
+    this.options.selected = newValue;
+    this._renderMenu();
+    this.options.afterChange(newValue);
+  },
+
+  _iconClicked: function() {
+    this._openMenu();
+  },
+
+  _itemClicked: function(item) {
+    console.log('item clicked', item);
+  },
+
+  onAdd: function(map) {
+    this.map = map;
+
+    this.container = L.DomUtil.create(
+      'div',
+      'leaflet-control leaflet-bar leaflet-control-select'
+    );
+    this.container.setAttribute('id', this.options.id);
+
+    const icon = L.DomUtil.create(
+      'a',
+      'leaflet-control-button fa ' +
+        this.options.icons.top +
+        ' ' +
+        this.options.additionalClass,
+      this.container
+    );
+
+    map.on('click', this._hideMenu, this);
+
+    L.DomEvent.on(icon, 'click', L.DomEvent.stop);
+    L.DomEvent.on(icon, 'click', this._iconClicked, this);
+
+    L.DomEvent.disableClickPropagation(this.container);
+    L.DomEvent.disableScrollPropagation(this.container);
+
+    this._render();
+    return this.container;
+  },
+
   _renderRadioIcon(selected, contentDiv) {
     const icons = this.options.icons;
     L.DomUtil.create(
@@ -60,112 +114,36 @@ L.Control.List = L.Control.extend({
     const selected = this._isSelected(item);
 
     const p = L.DomUtil.create('p', '', this.menu);
-    const pContent = L.DomUtil.create('div', p);
+    const pContent = L.DomUtil.create('div', '', p);
     const textSpan = L.DomUtil.create('span', '', pContent);
     textSpan.innerHTML = item.label;
 
     this._group
-      ? this._renderGroupIcon(item, pContent)
-      : this._renderRadioIcon(item, pContent);
+      ? this._renderGroupIcon(selected, pContent)
+      : this._renderRadioIcon(selected, pContent);
 
-    L.DomEvent.addListener(line, 'click', e => {
-      this._changeSelected(value);
-      this._openMenu();
+    L.DomEvent.addListener(p, 'click', e => {
+      this._itemClicked(item);
     });
 
     return p;
   },
 
-  onAdd: function(map) {
-    this.map = map;
-
-    this.map.options.selectControls
-      ? this.map.options.selectControls.push(this)
-      : (this.map.options.selectControls = [this]);
-
-    this.map._hideAllSelectControls = () => {
-      map.options.selectControls.map(selectControl =>
-        selectControl._hideMenu()
-      );
-    };
-
-    this.container = L.DomUtil.create(
-      'div',
-      'leaflet-control leaflet-bar leaflet-control-select'
-    );
-    this.container.setAttribute('id', this.options.id);
-    const icon = L.DomUtil.create(
-      'a',
-      'leaflet-control-button fa ' +
-        this.options.icon +
-        ' ' +
-        this.options.additionalClass,
-      this.container
-    );
+  _renderMenu() {
     this.menu = L.DomUtil.create(
       'div',
-      'leaflet-bar leaflet-control-select-menu',
+      'leaflet-control-select-menu leaflet-bar',
       this.container
     );
-
-    if (!this.state.menuOpen) {
-      map.on('click', this._hideMenu, this);
-
-      if (!L.Browser.mobile) {
-        L.DomEvent.on(
-          this.container,
-          {
-            mouseenter: this._openMenu,
-            mouseleave: this._hideMenu
-          },
-          this
-        );
-      }
-    }
-
-    if (L.Browser.touch) {
-      L.DomEvent.on(icon, 'click', L.DomEvent.stop);
-      L.DomEvent.on(icon, 'click', this._openMenu, this);
-    } else {
-      L.DomEvent.on(icon, 'focus', this._hideMenu, this);
-    }
-
-    L.DomEvent.disableClickPropagation(this.container);
-    L.DomEvent.disableScrollPropagation(this.container);
-
-    this._renderMenu();
-    return this.container;
-  },
-  _renderMenu: function() {
-    this.menu.innerHTML = '';
-
-    if (this.state.menuOpen) {
-      this.options.choices.map(option => {
-        this._createRadio(option, option === this.options.selected);
-      });
-      this.menu.className = 'leaflet-bar leaflet-control-select-menu open';
-    } else {
-      this.menu.className = 'leaflet-bar leaflet-control-select-menu closed';
-    }
+    this.options.items.map(item => {
+      this._renderItem(item);
+    });
+    return false;
   },
 
-  _hideMenu: function() {
-    this.state.menuOpen = false;
-    L.DomUtil.removeClass(this.container, 'leaflet-control-select-expanded');
-    this._renderMenu();
-  },
-
-  _openMenu: function() {
-    this.map._hideAllSelectControls();
-    L.DomUtil.addClass(this.container, 'leaflet-control-select-expanded');
-    this.state.menuOpen = true;
-    this._renderMenu();
-  },
-
-  _changeSelected: function(newValue) {
-    this.options.selected = newValue;
-    this._renderMenu();
-    this.options.afterChange(newValue);
+  _render: function() {
+    this.menu ? this.menu.remove() : false;
+    this.state.open ? this._renderMenu() : false;
   }
 });
 
